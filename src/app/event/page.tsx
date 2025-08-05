4758719bc1055c665185955041996b805bce3144
Hrishik Shaji <hrishikminishaji@gmail.com>
2025-08-05 14:24:32 +0530

    added local saving for resume chat

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -1,11 +1,10 @@
 import ChatComponent from "@/chatbot/components/ChatComponent";
-import WorkingResumableChat from "@/chatbot/components/ResumableChat";
+import WorkingResumableChat from "@/chatbot/components/LocalResume";
 
 export default function Page() {
   return (
     <div>
       <WorkingResumableChat />
-
     </div>
   )
 }
